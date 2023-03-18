d1c330fc6b1b5ad1645f5106bc73c5d81a0cf81f
yair8520 <yair8520@gmail.com>
2023-03-18 18:11:27 +0200

    fix login page scrollView  PersistTaps

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -6,7 +6,7 @@ import styles from './LoginStyles';
 import { Layout } from '../../Components/Layout';
 import { NInput, NText } from '../../Components';
 import { Button } from 'react-native-paper';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { createAccount, signInToFirebase } from '../../Helpers/FireBase';
 import { validateFields } from './helpers';
 import Lottie from 'lottie-react-native';
@@ -39,66 +39,68 @@ export const Login = ({}: LoginProps) => {
   };
   return (
     <Layout>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Lottie
-            source={require('../../Assets/Images/LoginAnim.json')}
-            autoSize
-            autoPlay
-            loop={true}
-            style={{ height: 250, width: 250 }}
-          />
-        </View>
-        <View style={styles.inputCon}>
-          <ShakeView error={!!info.errorEmail}>
-            <NInput
-              error={!!info.errorEmail}
-              label="Email "
-              errorText={info.errorEmail}
-              onChange={(e) => onChange('email', e)}
-              value={info.email}
+      <ScrollView keyboardShouldPersistTaps={'handled'}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Lottie
+              source={require('../../Assets/Images/LoginAnim.json')}
+              autoSize
+              autoPlay
+              loop={true}
+              style={{ height: 250, width: 250 }}
             />
-          </ShakeView>
-          <ShakeView error={!!info.errorPass}>
-            <NInput
-              secure={true}
-              error={!!info.errorPass}
-              label="Password"
-              errorText={info.errorPass}
-              onChange={(e) => onChange('pass', e)}
-              value={info.pass}
-            />
-          </ShakeView>
+          </View>
+          <View style={styles.inputCon}>
+            <ShakeView error={!!info.errorEmail}>
+              <NInput
+                error={!!info.errorEmail}
+                label="Email "
+                errorText={info.errorEmail}
+                onChange={(e) => onChange('email', e)}
+                value={info.email}
+              />
+            </ShakeView>
+            <ShakeView error={!!info.errorPass}>
+              <NInput
+                secure={true}
+                error={!!info.errorPass}
+                label="Password"
+                errorText={info.errorPass}
+                onChange={(e) => onChange('pass', e)}
+                value={info.pass}
+              />
+            </ShakeView>
 
-          <View style={styles.buttonCon}>
-            <SlideView dependency={signIn} side={signIn ? 'left' : 'right'}>
-              <Button
-                loading={loading}
-                disabled={loading}
-                onPress={signHandler}
-                buttonColor={'#367cff'}
-                mode="contained"
-                style={styles.button}
-              >
-                <NText variant="H3" style={styles.text}>
-                  {signIn ? 'Create account' : 'Sign In'}
-                </NText>
-              </Button>
-
-              <View style={styles.subText}>
-                <TouchableOpacity
-                  style={styles.SignIn}
-                  onPress={() => setSignIn(!signIn)}
+            <View style={styles.buttonCon}>
+              <SlideView dependency={signIn} side={signIn ? 'left' : 'right'}>
+                <Button
+                  loading={loading}
+                  disabled={loading}
+                  onPress={signHandler}
+                  buttonColor={'#367cff'}
+                  mode="contained"
+                  style={styles.button}
                 >
-                  <NText bold style={{ color: '#367cff' }}>
-                    {!signIn ? 'Create account' : 'Sign In'}
+                  <NText variant="H3" style={styles.text}>
+                    {signIn ? 'Create account' : 'Sign In'}
                   </NText>
-                </TouchableOpacity>
-              </View>
-            </SlideView>
+                </Button>
+
+                <View style={styles.subText}>
+                  <TouchableOpacity
+                    style={styles.SignIn}
+                    onPress={() => setSignIn(!signIn)}
+                  >
+                    <NText bold style={{ color: '#367cff' }}>
+                      {!signIn ? 'Create account' : 'Sign In'}
+                    </NText>
+                  </TouchableOpacity>
+                </View>
+              </SlideView>
+            </View>
           </View>
         </View>
-      </View>
+      </ScrollView>
     </Layout>
   );
 };
